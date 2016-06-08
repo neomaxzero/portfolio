e8cd1e66b49b38eee425a4c0ca0b366c13032d96
Max <maxicespedes7@hotmail.com>
2016-06-07 23:46:23 -0300

    Added my picture >D...

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
+    <div class="profilePicture">
+      <img src="../assets/me.jpg" alt="Me!" width="40%" />
+    </div>
       <p>{{msg}}</p>
   </div>
 </template>
@@ -12,7 +15,7 @@ export default {
       // with hot-reload because the reloaded component
       // preserves its current state and we are modifying
       // its initial state.
-      msg: 'About!!'
+      msg: 'Hi! Call me Maxi!'
     }
   }
 }
@@ -21,6 +24,19 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .about{
+      display: flex;
       height: 80%;
+      justify-content: center;
+      align-items: center;
+  }
+  .profilePicture{
+    margin: 1em;
+
+    border-radius: 10px;
+
+  }
+
+  img{
+    border: 1px solid white;
   }
 </style>
