95ca2600843f02e64990a801c36f490f282ccb49
Max <maxicespedes7@hotmail.com>
2019-08-17 09:28:37 +0200

    fix link to blog

--- src/components/AppLayout/Header/Header.vue
+++ src/components/AppLayout/Header/Header.vue
@@ -1,26 +1,26 @@
 <template>
-   <Header>
-     <UL>
-       <router-link to="/about">
-         <item>about</item>
-       </router-link>
-       <a href="https://blog.m4x.com.ar">
-         <item>blog</item>
-       </a>
-     </UL>
-     <slot></slot>
-   </Header>
+  <Header>
+    <UL>
+      <router-link to="/about">
+        <item>about</item>
+      </router-link>
+      <a href="https://blog.m4x.io">
+        <item>blog</item>
+      </a>
+    </UL>
+    <slot></slot>
+  </Header>
 </template>
 
 <script>
-import Header, { item, options as UL } from './Header';
+import Header, { item, options as UL } from "./Header";
 
 export default {
-  name: 'headerZ',
+  name: "headerZ",
   components: {
     Header,
     item,
-    UL,
-  },
+    UL
+  }
 };
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,36 +1,41 @@
 <template>
   <AppLayout>
     <Centerer column>
-      <P>
-        {{text}}
-      </P>
+      <P>{{text}}</P>
       <UL horizontal>
-        <li><A href="https://github.com/neomaxzero" alt="github" target="_blank" >github</A></li>
-        <li><A href="https://www.linkedin.com/in/neomaxzero/" alt="github" target="_blank" >linkedin</A></li>
-        <li><A href="https://500px.com/neomaxzero" alt="github" target="_blank" >500px</A></li>
-        <li><A href="mailto:maxicespedes7@hotmail.com" alt="email">email</A></li>
+        <li>
+          <A href="https://github.com/neomaxzero" alt="github" target="_blank">github</A>
+        </li>
+        <li>
+          <A href="https://www.linkedin.com/in/neomaxzero/" alt="github" target="_blank">linkedin</A>
+        </li>
+        <li>
+          <A href="https://500px.com/neomaxzero" alt="github" target="_blank">500px</A>
+        </li>
+        <li>
+          <A href="mailto:maxicespedes7@hotmail.com" alt="email">email</A>
+        </li>
       </UL>
     </Centerer>
   </AppLayout>
 </template>
 
 <script>
-import AppLayout from '@/components/AppLayout/AppLayout.vue';
-import Centerer from '@/Utils/Centerer';
-import DU from '@/components/DesignUnits';
+import AppLayout from "@/components/AppLayout/AppLayout.vue";
+import Centerer from "@/Utils/Centerer";
+import DU from "@/components/DesignUnits";
 
-console.log(DU);
 export default {
-  name: 'home',
+  name: "home",
   components: {
     AppLayout,
     Centerer,
-    ...DU,
+    ...DU
   },
   data() {
     return {
-      text: 'your curiosity leads you here...theres no way back',
+      text: "your curiosity leads you here...theres no way back"
     };
-  },
+  }
 };
 </script>
